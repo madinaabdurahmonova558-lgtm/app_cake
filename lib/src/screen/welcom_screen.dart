77cd7ca664c99eaaf18e9f745c4c01f7548cada4
madinaabdurahmonova558-lgtm <madinaabdurahmonova558@gmail.com>
2026-05-02 14:58:55 +0600

    sdfg

---
+++ lib/src/screen/welcom_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class WelcomScreen extends StatelessWidget {
+  const WelcomScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
